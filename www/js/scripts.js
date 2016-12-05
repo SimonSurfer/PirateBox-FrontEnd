c47e8ea56229134d92b5a2a4980f90a6645d4988
dsbrooks <david@dsbrooks.me>
2016-12-05 14:46:27 +0900

    removed desktop version for now, added identicons and timeago

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -1,75 +1,87 @@
 $(document).ready(function() {
- 	$.get('forum.html', function(data) {
-      $('div#forum_link').html(data);
-  });
+    $.get('forum.html', function(data) {
+        $('div#forum_link').html(data);
+    });
 
- 	$.get('forban_link.html', function(data) {
-      $('div#forban_link').html(data);
-  });
+    $.get('forban_link.html', function(data) {
+        $('div#forban_link').html(data);
+    });
 
-  $.get('station_cnt.txt', function(data) {
-      $('div#station').html(data);
-  });
+    $.get('station_cnt.txt', function(data) {
+        $('div#station').html(data);
+    });
 
-  display_shoutbox();
-  
- 	/* shoutbox disabled while testing
-  $('div#shoutbox').ajaxError(function() {
-      $(this).text( "Triggered ajaxError handler on shoutbox" );
-  });
-  */
+    display_shoutbox();
 
-  $("#sb_form").submit(function(event) {
-    /* stop form from submitting normally */
-      event.preventDefault();
-    post_shoutbox();
-  });
+    /* shoutbox disabled while testing
+    $('div#shoutbox').ajaxError(function() {
+        $(this).text( "Triggered ajaxError handler on shoutbox" );
+    });
+    */
 
-  // spin menu icon and toggle nav
-  $('#menu-icon').click(function() {
-      $(this).toggleClass('rotate');
-      $('#top-nav').slideToggle();
-  });
+    $("#sb_form").submit(function(event) {
+        /* stop form from submitting normally */
+        event.preventDefault();
+        post_shoutbox();
+    });
 
-  // closes mobile nav
-  $('#top-nav a').click(function() {
-      if ($('#top-nav').is(':visible')
-      && $('#menu-icon').is(':visible')) {
-          $('#top-nav').slideUp();
-          $('#menu-icon').toggleClass('rotate');
-      }
-  });
+    // spin menu icon and toggle nav
+    $('#menu-icon').click(function() {
+        $(this).toggleClass('rotate');
+        $('#top-nav').slideToggle();
+    });
 
-  // hides welcome
-  $('#thanks').click(function() {
-      $('#welcome').slideUp();
-  });
+    // closes mobile nav
+    $('#top-nav a').click(function() {
+        if ($('#top-nav').is(':visible')
+        && $('#menu-icon').is(':visible')) {
+            $('#top-nav').slideUp();
+            $('#menu-icon').toggleClass('rotate');
+        }
+    });
 
-  // fab
-  var links = [
-    {
-        "bgcolor":"rgb(173,0,11);",
-        "icon":"+"
-    },
-    {
-        "bgcolor":"rgb(27,29,30);",
-        "color":"fffff",
-        "icon":"M",
-    },
-    {
-        "bgcolor":"rgb(27,29,30);",
-        "color":"#fffff",
-        "icon":"U"
-    }
-  ];
-  var options = {
-      rotate: true
-  };
-  $('#fab').jqueryFab(links, options);
+    // hides welcome
+    $('#modal-welcome .button').click(function() {
+        $('#welcome').slideUp();
+    });
 
-  // message character counter
-  updateCountdown();
-  $('.message').on('input', updateCountdown);
+    // fab
+    var links = [
+        {
+            "bgcolor":"rgb(173,0,11);",
+            "icon":"+"
+        },
+        {
+            "bgcolor":"rgb(27,29,30);",
+            "color":"fffff",
+            "icon":"M",
+        },
+        {
+            "bgcolor":"rgb(27,29,30);",
+            "color":"#fffff",
+            "icon":"U"
+        }
+    ];
+    var options = {
+        rotate: true
+    };
+    $('#fab').jqueryFab(links, options);
+
+    // message character counter
+    updateCountdown();
+    $('.message').on('input', updateCountdown);
+
+    // populate the shoutbox with spam for testing
+    for (var i = 20; i > 0; i--) {
+        $('#shoutbox').append('<div class="message"><div class="identicon">Nickname' + i + '</div><div class="message-info"><name>Nickname' + i + '</name> <date class="timeago" title="2016-12-04 ' + i + ':38:00"></date></div><div class-"message-data"><data class="def">Lorem ipsum dolor sit amet ipsum dolor sit amet ipsum dolor sit amet ipsum dolor sit amet ipsum dolor sit amet ipsum dolor sit amet</data></div></div>');
+    };
+
+    //timeago
+    $('date.timeago').timeago();
+
+    // identicons
+    identicons();
+    $('.identicon').identicon5();
 });
 
 function refresh_shoutbox () {
@@ -101,7 +113,15 @@ function fnGetDomain(url) {
 }
 
 function updateCountdown() {
-  // 140 is the max message length
-  var remaining = 140 - $('.message').val().length;
-  $('.counter').text(remaining + ' characters remaining.');
+    // 140 is the max message length
+    var remaining = 140 - $('.message').val().length;
+    $('.counter').text(remaining + ' characters remaining.');
+}
+
+function identicons() {
+    $('.identicon').each(function() {
+        var tohash = $(this).html();
+        var hashed = $.md5(tohash);
+        $(this).html(hashed);
+    });
 }
